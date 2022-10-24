61c9b924bd293a7cfb2c6a7737b69098baba3120
Rezal Astana <rezalastana21@gmail.com>
2022-10-24 16:48:12 +0700

    update name

--- database02/src/main/java/com/database02/Database02Application.java
+++ database02/src/main/java/com/database02/Database02Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class Database02Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(Database02Application.class, args);
 	}
 
 }
